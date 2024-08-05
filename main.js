52ff934e94c9129f6f108eea4f3757ce41fc7f52
Rasim-1 <husanbaevrasim315@gmail.com>
2024-08-05 12:10:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('.btn-course').addEventListener('click', () => {
+
+    document.querySelector('.btn-course').classList.toggle('active')
+
+    document.querySelector('.dropdown__about').classList.toggle('active')
+})
